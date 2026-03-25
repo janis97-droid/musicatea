47be72ebe1484e4317f8d1cdafd63a7c696b4a80
janis97-droid <jawadabuyounis97@gmail.com>
2026-03-25 19:17:40 +0200

    Update sheets.js

--- data/sheets.js
+++ data/sheets.js
@@ -1,1 +1,35 @@
-const sheets=[{title:"سماعي نهاوند",system:"arabic",type:"instrumental",composer:"مسعود جميل",maqam:"نهاوند",tonic:"دو",difficulty:"متقدم"},{title:"شكراً",system:"arabic",type:"song",composer:"وليد سعد",performer:"جورج وسوف",maqam:"كرد",tonic:"صول",difficulty:"متقدم"},{title:"Cinema Paradiso",system:"western",type:"instrumental",composer:"Ennio Morricone",scale:"Bb Major",tonic:"Bb",difficulty:"متقدم"}];
+const sheets = [
+  {
+    title: "سماعي نهاوند",
+    system: "arabic",
+    type: "instrumental",
+    composer: "مسعود جميل",
+    performer: null,
+    maqam: "نهاوند",
+    scale: null,
+    tonic: "دو",
+    difficulty: "متقدم"
+  },
+  {
+    title: "شكراً",
+    system: "arabic",
+    type: "song",
+    composer: "وليد سعد",
+    performer: "جورج وسوف",
+    maqam: "كرد",
+    scale: null,
+    tonic: "صول",
+    difficulty: "متقدم"
+  },
+  {
+    title: "Cinema Paradiso",
+    system: "western",
+    type: "instrumental",
+    composer: "Ennio Morricone",
+    performer: null,
+    maqam: null,
+    scale: "Bb Major",
+    tonic: "Bb",
+    difficulty: "متقدم"
+  }
+];
