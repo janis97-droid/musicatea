d4ec7d95fe33508c8d03c223d31fea77923672ca
janis97-droid <jawadabuyounis97@gmail.com>
2026-03-25 21:32:15 +0200

    Update sheets.js

--- data/sheets.js
+++ data/sheets.js
@@ -1,6 +1,7 @@
 const sheets = [
+
   {
-    title: "سماعي نهاوند",
+    title: "سماعي نهاوند (دو)",
     system: "arabic",
     type: "instrumental",
     composer: "مسعود جميل",
@@ -10,6 +11,19 @@ const sheets = [
     tonic: "دو",
     pdf: "pdfs/samai-nahawand-do.pdf"
   },
+
+  {
+    title: "سماعي نهاوند (ري)",
+    system: "arabic",
+    type: "instrumental",
+    composer: "مسعود جميل",
+    performer: null,
+    maqam: "نهاوند",
+    scale: null,
+    tonic: "ري",
+    pdf: "pdfs/samai-nahawand-re.pdf"
+  },
+
   {
     title: "شكراً",
     system: "arabic",
@@ -21,6 +35,7 @@ const sheets = [
     tonic: "صول",
     pdf: "pdfs/shukran.pdf"
   },
+
   {
     title: "Cinema Paradiso",
     system: "western",
@@ -31,5 +46,198 @@ const sheets = [
     scale: "Bb Major",
     tonic: "Bb",
     pdf: "pdfs/cinema-paradiso.pdf"
+  },
+
+  {
+    title: "عالروزانا",
+    system: "arabic",
+    type: "song",
+    composer: "عثمان الموصلي",
+    performer: "تراث",
+    maqam: "راست",
+    scale: null,
+    tonic: "دو",
+    pdf: "pdfs/alrozana.pdf"
+  },
+
+  {
+    title: "أنا قلبي دليلي (دو)",
+    system: "arabic",
+    type: "song",
+    composer: "محمد القصبجي",
+    performer: "ليلى مراد",
+    maqam: "عجم",
+    scale: null,
+    tonic: "دو",
+    pdf: "pdfs/ana-qalbi-do.pdf"
+  },
+
+  {
+    title: "أنا قلبي دليلي (فا)",
+    system: "arabic",
+    type: "song",
+    composer: "محمد القصبجي",
+    performer: "ليلى مراد",
+    maqam: "عجم",
+    scale: null,
+    tonic: "فا",
+    pdf: "pdfs/ana-qalbi-fa.pdf"
+  },
+
+  {
+    title: "أنا وليلى",
+    system: "arabic",
+    type: "song",
+    composer: "كاظم الساهر",
+    performer: "كاظم الساهر",
+    maqam: "كرد",
+    scale: null,
+    tonic: "لا",
+    pdf: "pdfs/ana-w-layla.pdf"
+  },
+
+  {
+    title: "أنشودة الفن (دو)",
+    system: "arabic",
+    type: "song",
+    composer: "محمد عبد الوهاب",
+    performer: "محمد عبد الوهاب",
+    maqam: "نهاوند",
+    scale: null,
+    tonic: "دو",
+    pdf: "pdfs/fan-do.pdf"
+  },
+
+  {
+    title: "أنشودة الفن (فا)",
+    system: "arabic",
+    type: "song",
+    composer: "محمد عبد الوهاب",
+    performer: "محمد عبد الوهاب",
+    maqam: "نهاوند",
+    scale: null,
+    tonic: "فا",
+    pdf: "pdfs/fan-fa.pdf"
+  },
+
+  {
+    title: "خسرت كل الناس",
+    system: "arabic",
+    type: "song",
+    composer: "وليد سعد",
+    performer: "جورج وسوف",
+    maqam: "كرد",
+    scale: null,
+    tonic: "صول",
+    pdf: "pdfs/khsert.pdf"
+  },
+
+  {
+    title: "ما في ورد",
+    system: "arabic",
+    type: "song",
+    composer: "نزار فرنسيس",
+    performer: "ملحم بركات",
+    maqam: "كرد",
+    scale: null,
+    tonic: "لا",
+    pdf: "pdfs/ma-fi-ward.pdf"
+  },
+
+  {
+    title: "ومنين أبدأ يا قلبي (ري)",
+    system: "arabic",
+    type: "song",
+    composer: "فادي صعب",
+    performer: "فادي صعب",
+    maqam: "بيات",
+    scale: null,
+    tonic: "ري",
+    pdf: "pdfs/mneen-re.pdf"
+  },
+
+  {
+    title: "ومنين أبدأ يا قلبي (صول)",
+    system: "arabic",
+    type: "song",
+    composer: "فادي صعب",
+    performer: "فادي صعب",
+    maqam: "بيات",
+    scale: null,
+    tonic: "صول",
+    pdf: "pdfs/mneen-sol.pdf"
+  },
+
+  {
+    title: "على مودك انت وبس (ري)",
+    system: "arabic",
+    type: "song",
+    composer: "رائد الشامي",
+    performer: "ماجد المهندس",
+    maqam: "بيات",
+    scale: null,
+    tonic: "ري",
+    pdf: "pdfs/modak-re.pdf"
+  },
+
+  {
+    title: "على مودك انت وبس (صول)",
+    system: "arabic",
+    type: "song",
+    composer: "رائد الشامي",
+    performer: "ماجد المهندس",
+    maqam: "بيات",
+    scale: null,
+    tonic: "صول",
+    pdf: "pdfs/modak-sol.pdf"
+  },
+
+  {
+    title: "قضية عم أحمد",
+    system: "arabic",
+    type: "instrumental",
+    composer: "عمر خيرت",
+    performer: null,
+    maqam: "كرد",
+    scale: null,
+    tonic: "ري",
+    pdf: "pdfs/om-ahmad.pdf"
+  },
+
+  {
+    title: "صحاك الشوق",
+    system: "arabic",
+    type: "song",
+    composer: "جمانة جمال",
+    performer: "فضل شاكر",
+    maqam: "بيات",
+    scale: null,
+    tonic: "ري",
+    pdf: "pdfs/sahak-el-shoq.pdf"
+  },
+
+  {
+    title: "سجر البن",
+    system: "arabic",
+    type: "song",
+    composer: "مارسيل خليفة",
+    performer: "مارسيل خليفة",
+    maqam: "نهاوند",
+    scale: null,
+    tonic: "دو",
+    pdf: "pdfs/sajar-el-bun.pdf"
+  },
+
+  {
+    title: "توبة",
+    system: "arabic",
+    type: "song",
+    composer: "محمد عبد الوهاب",
+    performer: "عبد الحليم حافظ",
+    maqam: "كرد",
+    scale: null,
+    tonic: "ري",
+    pdf: "pdfs/touba.pdf"
   }
+
 ];
