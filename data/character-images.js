5dd2ad59d18d7e4517e97da4bd94beef8f8da3d8
Jawad.HTMusL <jawadabuyounis97@gmail.com>
2026-04-23 01:45:51 +0300

    Add character image map for person pages

---
+++ data/character-images.js
@@ -0,0 +1,12 @@
+const characterImages = {
+  "umm-kulthum": {
+    main_image: "assets/history/characters/umm-kulthum/16-4.jpg",
+    main_image_caption: "أم كلثوم في صورة بورتريه من مرحلتها الفنية المبكرة.",
+    extra_images: [
+      {
+        file: "assets/history/characters/umm-kulthum/Kulthum.jpg",
+        caption: "أم كلثوم في أداء حي على المسرح."
+      }
+    ]
+  }
+};
